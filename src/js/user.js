52328328f3958daa8823ee368ed24d58c7d62b65
Aluisio Lima <aaluisio309@gmail.com>
2025-01-30 20:14:36 -0300

    feat(produtos): criada a tela de produto pra admin mais ainda sem as funcionalidades totais so design

--- src/js/user.js
+++ src/js/user.js
@@ -4,7 +4,7 @@ async function getUsers() {
 
     const div = document.createElement("div");
     div.id = "cards";
-    div.className = "users_div";
+    div.className = "_div";
     document.getElementById("options-cards").appendChild(div)
     
     if (!users.error && users.data.length !== 0) {
