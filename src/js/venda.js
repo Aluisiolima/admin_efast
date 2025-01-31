1f262c6e4b4807f9ae246861a8d2a2cf23b344ef
Aluisio Lima <aaluisio309@gmail.com>
2025-01-30 21:36:18 -0300

    feat(vendas): criada a tela para ver as vendas na tel ade admin ainda sem as funcionalidades completas

---
+++ src/js/venda.js
@@ -0,0 +1,23 @@
+async function getVendas() {
+    const vendas = await fetchApi([], "POST", `${link_api}/pegarVendas/`);
+    
+    if (!vendas.error && vendas.data.length !== 0) {
+        document.getElementById("options-cards").innerHTML = "";
+        const div = document.createElement("div");
+        div.id = "cards";
+        div.className = "_div2";
+        document.getElementById("options-cards").appendChild(div)
+
+        for (const chave in vendas.data){
+            let icon = "<i class='bi bi-check-lg'></i>";
+            let color = "green"
+            if (vendas.data[chave].status === "pendente"){
+                icon = "<i class='bi bi-x-lg'></i>"
+                color = "red"
+            }
+            vendas.data[chave].status = icon;
+            vendas.data[chave].color = color;
+            render("components/cards-venda.html", vendas.data[chave], "cards");
+        }
+    }
+}
