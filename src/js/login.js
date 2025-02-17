3a34592733613c4c8a423765c2e74425e5e4f7d8
Aluisio Lima <aaluisio309@gmail.com>
2025-02-17 11:56:44 -0300

    refactor(cod): code styles

--- src/js/login.js
+++ src/js/login.js
@@ -1,27 +1,27 @@
 async function login(button) {
-    const form = button.form;
+  const form = button.form;
 
-    document.getElementById("form").addEventListener("submit", function (event) {
-        event.preventDefault();
-    });
+  document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
 
-    const formData = new FormData(form);
+  const formData = new FormData(form);
 
-    const dados = {};
-    formData.forEach((value, key) => {
-        dados[key] = value;
-    });
+  const dados = {};
+  formData.forEach((value, key) => {
+    dados[key] = value;
+  });
 
-    const result = await fetchApi(dados, "POST", `${link_api}/login`);
+  const result = await fetchApi(dados, "POST", `${link_api}/login`);
 
-    if (!result.error) {
-        sessionStorage.setItem('token', result.data.token);
-        await render("components/animacao.html", null, "container");
+  if (!result.error) {
+    sessionStorage.setItem("token", result.data.token);
+    await render("components/animacao.html", null, "container");
 
-        setTimeout(async () => {
-            document.getElementById("container").innerHTML = "";
-            await render("components/container-desktop.html", null, "container");
-            await getUsers();
-        }, 2500);
-    }
+    setTimeout(async () => {
+      document.getElementById("container").innerHTML = "";
+      await render("components/container-desktop.html", null, "container");
+      await getUsers();
+    }, 2500);
+  }
 }
