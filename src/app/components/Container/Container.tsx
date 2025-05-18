8904a4d15466efc095f9c23f749fb16780f30eeb
Aluisio Lima <aaluisio309@gmail.com>
2025-05-18 11:17:50 -0300

    format(): Container

--- src/app/components/Container/Container.tsx
+++ src/app/components/Container/Container.tsx
@@ -16,7 +16,7 @@ import { Arquivos } from "../Arquivos/Arquivos";
 import { Arquivo } from "../../types/Arquivo.type";
 import { ContainerDesktop } from "./ContainerDesktop";
 import { ContainerMobile } from "./ContainerMobile";
-import {} from "../../asset"
+import {} from "../../asset";
 
 export const Container: React.FC<{ dataUser: LoginType }> = ({ dataUser }) => {
   useJWTToken(dataUser);
